1e26b0bda4eba94eaf746507cb824a677a17da98
hyukhur <hyukhur@gmail.com>
2013-03-19 08:00:10 +0900

    define macro for the class validation and a compatible class loading

--- HHCompatibility/HHCompatibility.h
+++ HHCompatibility/HHCompatibility.h
@@ -12,3 +12,5 @@
 - (BOOL)respondsToFunction:(NSString *)aFunctionName inFramwork:(NSString *)aFrameworkName;
 - (id)performFunction:(NSString *)aFunctionName inFramwork:(NSString *)aFrameworkName;
 @end
+
+#define HHClass(className) [className class] ? [className class] : NSClassFromString(@#className)
